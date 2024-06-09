0adf36137fe8a1e44300f13bb9be44422062e78c
NikolicAleksandar <vojvodaaleksandar11@gmail.com>
2024-06-09 18:42:01 +0200

    next-auth v5 github provider

---
+++ app/(auth)/github/page.tsx
@@ -0,0 +1,10 @@
+import SignIn from "@/components/auth/github";
+import React from "react";
+
+export default function GitHubPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center ">
+      <SignIn />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 export default function Home() {
+  // hono rpc/ drizzle schema into hono routes into hono client into tanstack query. fullstack typesafety.
+  // drizzle zod for shadcn react hook forms.
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       <h1 className="text-4xl font-bold ">Next Auth v5</h1>
